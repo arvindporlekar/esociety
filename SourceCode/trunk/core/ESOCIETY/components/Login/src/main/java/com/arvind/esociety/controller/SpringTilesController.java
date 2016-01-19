348b8b43d0528e7186b87e6b8ee7d2b9be8c73ad
Arvind Porlekar <Arvind.Porlekar@Indussoft.com>
2016-01-19 11:42:31 +0530

    Project structure

---
+++ SourceCode/trunk/core/ESOCIETY/components/Login/src/main/java/com/arvind/esociety/controller/SpringTilesController.java
@@ -0,0 +1,29 @@
+package com.arvind.esociety.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.arvind.esociety.vo.Person;
+
+@Controller
+public class SpringTilesController {
+
+	@RequestMapping(value="index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="viewPeson")
+	public ModelAndView viewPersons(Model model) {
+		Map<String, List<Person>> persons = 
+				new HashMap<String, List<Person>>();
+		persons.put("persons", Person.createPersons());
+		return new ModelAndView("personList", persons);
+	}
+}
